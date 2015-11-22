d5431772ce4bc578ac53e6db1808b78fd0545282
AgenricWon <AgenricWon@gmail.com>
2015-11-22 13:43:24 +0800

    Initial Commit

---
+++ AGBottomDock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AGBottomDock
+//
+//  Created by Agenric on 15/11/22.
+//  Copyright © 2015年 Agenric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AGBottomDock/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AGBottomDock
+//
+//  Created by Agenric on 15/11/22.
+//  Copyright © 2015年 Agenric. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
